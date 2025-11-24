026311955b5a3165260897cad641894e78fd79dc
arka <arkakar1313@gmail.com>
2025-11-24 12:48:10 +0530

    Split the main apps into cli and gui. Basic structure of gui initiated

---
+++ Code-Snippets/12_guess-the-number.py
@@ -0,0 +1,25 @@
+import random
+
+upper_limit = int(input("Choose an upper limit: "))
+lower_limit = int(input("Choose an lower limit: "))
+
+random_number = random.randint(lower_limit, upper_limit + 1)
+
+guesses = 1
+guess_limit = 3
+
+while guesses <= guess_limit:
+    guess_number = int(input("Guess the number: "))
+    if guess_number == random_number:
+        break
+    elif guess_number < random_number:
+        print("Actual number is higher than you guessed.")
+    else:
+        print("Actual number is lower than you guessed.")
+    guesses += 1
+
+if guesses <= guess_limit:
+    print("\nYou have guessed the correct number.")
+else:
+    print("\nYou have failed to guess\n"
+          "The correct number was: ", random_number)
---
+++ Todo-App/gui_main.py
@@ -0,0 +1,10 @@
+import modules.functions
+import FreeSimpleGUI as sg
+
+label = sg.Text("Type in a todo: ")
+input_box = sg.InputText(tooltip="Enter todo")
+add_button = sg.Button("Add")
+
+window = sg.Window("Todo app", layout=[[label], [input_box, add_button]])
+window.read()
+window.close()
