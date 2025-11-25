d6301f9e181c85f9f4974c00a2a327c84f962cc3
arka <arkakar1313@gmail.com>
2025-11-25 18:17:45 +0530

    Added

---
+++ Code-Snippets/13_file_compressor_gui.py
@@ -0,0 +1,9 @@
+import FreeSimpleGUI as sg
+
+layout = [[sg.Text("Select files to compress: "), sg.Input(), sg.FilesBrowse("Choose")],
+          [sg.Text("Select destination folder: "), sg.Input(), sg.FolderBrowse("Choose")],
+          [sg.Button("Compress")]]
+
+window = sg.Window("File Zipper", layout)
+window.read()
+window.close()
