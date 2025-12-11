9f344589e5f044de856c7ba0bdddb4b18be336d0
arka <arkakar1313@gmail.com>
2025-12-11 18:20:04 +0530

    Implement "Add Todo" feature

--- Todo-App/web_main.py
+++ Todo-App/web_main.py
@@ -1,11 +1,22 @@
+from linecache import clearcache
+
 import streamlit as st
 import modules.functions as fns
 
 todos = fns.get_todos_from_file()
+
+def add_todo():
+    todo_to_be_added = st.session_state["new_todo"]
+    todos.append(todo_to_be_added + "\n")
+    fns.write_todos(todos)
+
 
 st.title("Todo App ")
 
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="", placeholder="Add new todo...")
+st.text_input(label=" ",
+              placeholder="Add new todo...",
+              on_change=add_todo,
+              key="new_todo")
