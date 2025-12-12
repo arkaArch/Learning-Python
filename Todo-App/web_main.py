70adbdc3226c716c2c1f65d7ab4e3ffafba45f2d
arka <arkakar1313@gmail.com>
2025-12-12 21:30:51 +0530

    Implement "Completed" feature

--- Todo-App/web_main.py
+++ Todo-App/web_main.py
@@ -1,7 +1,9 @@
-from linecache import clearcache
-
 import streamlit as st
 import modules.functions as fns
+
+# You can deploy this app by create a new project with pycharm and build only streamlit app ( i.e. web_main.py with
+# ofcourse function.py, todos.txt and requirements.txt) and upload the project into github and deploy to streamlit
+# community from the "Deploy" button in the streamlit app in your browser.
 
 todos = fns.get_todos_from_file()
 
@@ -13,10 +15,20 @@ def add_todo():
 
 st.title("Todo App ")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    # Check if a checkbox is checked
+    if checkbox:
+        # Remove the checked to-do from todos list
+        todos.pop(index)
+        # Write the updated todos in the file
+        fns.write_todos(todos)
+        # Also delete the to-do from the session state
+        del st.session_state[todo]
+        # Rerun the entire streamlit app
+        st.rerun()
 
 st.text_input(label=" ",
               placeholder="Add new todo...",
               on_change=add_todo,
-              key="new_todo")
+              key="new_todo")   # We use key for session_state
